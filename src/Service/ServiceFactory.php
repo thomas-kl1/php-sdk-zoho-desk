395add40b4a45f04b4ff2d1ef22fabe68a2b501b
Thomas Klein <thomas.klein@mpbio.com>
2021-12-13 10:52:22 +0100

    add nullable params

--- src/Service/ServiceFactory.php
+++ src/Service/ServiceFactory.php
@@ -22,27 +22,27 @@ final class ServiceFactory
         $this->dataObjectFactory = $dataObjectFactory;
     }
 
-    public function createOperation(string $entityType, ?string $path, array $arguments = []): CreateOperationInterface
+    public function createOperation(string $entityType, ?string $path = null, array $arguments = []): CreateOperationInterface
     {
         return new CreateOperation($this->requestBuilder, $this->dataObjectFactory, $entityType, $path, $arguments);
     }
 
-    public function readOperation(string $entityType, ?string $path, array $arguments = []): ReadOperationInterface
+    public function readOperation(string $entityType, ?string $path = null, array $arguments = []): ReadOperationInterface
     {
         return new ReadOperation($this->requestBuilder, $this->dataObjectFactory, $entityType, $path, $arguments);
     }
 
-    public function updateOperation(string $entityType, ?string $path, array $arguments = []): UpdateOperationInterface
+    public function updateOperation(string $entityType, ?string $path = null, array $arguments = []): UpdateOperationInterface
     {
         return new UpdateOperation($this->requestBuilder, $this->dataObjectFactory, $entityType, $path, $arguments);
     }
 
-    public function deleteOperation(string $entityType, ?string $path, array $arguments = []): DeleteOperationInterface
+    public function deleteOperation(string $entityType, ?string $path = null, array $arguments = []): DeleteOperationInterface
     {
         return new DeleteOperation($this->requestBuilder, $entityType, $path, $arguments);
     }
 
-    public function listOperation(string $entityType, ?string $path, array $arguments = []): ListOperationInterface
+    public function listOperation(string $entityType, ?string $path = null, array $arguments = []): ListOperationInterface
     {
         return new ListOperation($this->requestBuilder, $this->dataObjectFactory, $entityType, $path, $arguments);
     }
